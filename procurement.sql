a9fbf1bac3e099a21c75a9bcdff9e2b2ac22134d
nkwine <105311313+nkwine@users.noreply.github.com>
2022-10-03 08:26:22 +0300

    new sql

--- procurement.sql
+++ procurement.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 01, 2022 at 02:30 PM
+-- Generation Time: Oct 03, 2022 at 07:24 AM
 -- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- PHP Version: 8.0.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -50,30 +50,10 @@ CREATE TABLE `applications` (
 --
 
 INSERT INTO `applications` (`id`, `a_id`, `tender`, `name`, `adress`, `person`, `title`, `email`, `contact`, `doc1`, `doc2`, `doc3`, `dat`, `user`, `price`) VALUES
-(1, 1, '', '', '', '', '', '', '', 'NEGOTIATION STRATEGY AND PLANNING.pptx', '', '', '2022-09-21', '', ''),
-(1, 2, 'chairs', '', '', '', '', '', '', 'NEGOTIATION STRATEGY AND PLANNING.pptx', '', '', '2022-09-21', '', ''),
 (1, 3, 'chairs', 'Muni', 'Arua', 'John', 'Manager', 'nkwineinnocent5@gmail.com', '0785287421', 'NEGOTIATION STRATEGY AND PLANNING.pptx', 'TOPIC 2 (1).pptx', 'NATURE OF NEGOTIATION (1).pptx', '2022-09-21', '', ''),
 (1, 4, 'chairs', 'Muni', 'Arua', 'John', 'manager', 'nkwineinnocent5@gmail.com', '0785287421', 'y2mate.com - Ebibi byangye Yesu Akabyekorera HYMN 185 Runyankole Rukiga Church Of Uganda (2).mp3', 'y2mate.com - 250 IRA NKABA NINGIRA NTIRUNYANKORERUKIGA HYMNS.mp3', 'y2mate.com - Ira kunabaire ngwejegiire Hati Igura Runyankole Rukiga Hymn with New English Lyrics Song.mp3', '2022-09-21', '', ''),
 (1, 5, 'chairs', 'Muni', 'Arua', 'John', 'manager', 'nkwineinnocent5@gmail.com', '0785287421', 'y2mate.com - Ebibi byangye Yesu Akabyekorera HYMN 185 Runyankole Rukiga Church Of Uganda (2).mp3', 'y2mate.com - 250 IRA NKABA NINGIRA NTIRUNYANKORERUKIGA HYMNS.mp3', 'y2mate.com - Ira kunabaire ngwejegiire Hati Igura Runyankole Rukiga Hymn with New English Lyrics Song.mp3', '2022-09-21', '', ''),
 (1, 6, 'chairs', 'Muni', 'Arua', 'John', 'V.C', '1901200153@muni.ac.ug', '0785287422', 'NEGOTIATION STRATEGY AND PLANNING.pptx', 'TOPIC 2 (1).pptx', 'NATURE OF NEGOTIATION (1).pptx', '2022-09-21', '', '');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `organizations`
---
-
-CREATE TABLE `organizations` (
-  `id` int(50) NOT NULL,
-  `oname` varchar(50) NOT NULL,
-  `oimage` varchar(50) NOT NULL,
-  `fname` varchar(50) NOT NULL,
-  `lname` varchar(50) NOT NULL,
-  `tel` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `pswd` varchar(50) NOT NULL,
-  `rp` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -100,8 +80,33 @@ CREATE TABLE `publish` (
 --
 
 INSERT INTO `publish` (`id`, `a_id`, `item`, `qty`, `type`, `specification`, `doc1`, `doc2`, `doc3`, `requirements`, `deadline`) VALUES
-(1, 1, 'chairs', '20	', '', '', '', '', '', '', '0000-00-00'),
 (1, 2, 'chairs', '20	', 'office chairs', 'They must be of medium height', 'license', 'tax clearance', 'financial statement', 'The tenders must have been in the business for more than 4 years', '2022-10-11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rating`
+--
+
+CREATE TABLE `rating` (
+  `id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `tender` varchar(100) NOT NULL,
+  `price` varchar(100) NOT NULL,
+  `services` varchar(100) NOT NULL,
+  `quality` varchar(100) NOT NULL,
+  `requirements` varchar(100) NOT NULL,
+  `comment` varchar(100) NOT NULL,
+  `official` varchar(100) NOT NULL,
+  `dat` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `rating`
+--
+
+INSERT INTO `rating` (`id`, `name`, `tender`, `price`, `services`, `quality`, `requirements`, `comment`, `official`, `dat`) VALUES
+(2, 'Muni', 'supply of  chairs', 'ugsh 5,000,000', '3', '3', '2', 'The supplier is not good', 'Iga Isaac', '2022-10-02');
 
 -- --------------------------------------------------------
 
@@ -118,37 +123,16 @@ CREATE TABLE `requests` (
   `dat` varchar(100) NOT NULL,
   `processor` varchar(100) NOT NULL,
   `department` varchar(100) NOT NULL,
-  `status` varchar(100) NOT NULL
+  `status` varchar(100) NOT NULL,
+  `org` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `requests`
 --
 
-INSERT INTO `requests` (`id`, `item`, `qty`, `purpose`, `requestor`, `dat`, `processor`, `department`, `status`) VALUES
-(1, 'chairs', '20', 'for lecturers in the staff', 'abdu', '2022-09-16 ', 'annet', 'Computer science', 'approved');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `suppliers`
---
-
-CREATE TABLE `suppliers` (
-  `email` varchar(60) NOT NULL,
-  `fname` varchar(60) NOT NULL,
-  `lname` varchar(50) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `tel` varchar(50) NOT NULL,
-  `pswd` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `suppliers`
---
-
-INSERT INTO `suppliers` (`email`, `fname`, `lname`, `username`, `tel`, `pswd`) VALUES
-('tikol@yahoo.com', 'tiko', 'lucy', 'lucyt', '0787009988', 'lucyt22');
+INSERT INTO `requests` (`id`, `item`, `qty`, `purpose`, `requestor`, `dat`, `processor`, `department`, `status`, `org`) VALUES
+(1, 'chairs', '20', 'for lecturers in the staff', 'abdu', '2022-09-16 ', 'annet', 'Computer science', 'approved', 'muni');
 
 -- --------------------------------------------------------
 
@@ -203,17 +187,18 @@ CREATE TABLE `users` (
   `contact` varchar(100) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `cod` int(11) NOT NULL
+  `cod` int(11) NOT NULL,
+  `org` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `fname`, `lname`, `department`, `position`, `email`, `contact`, `username`, `password`, `cod`) VALUES
-(1, 'Nkwine', 'Innocent', 'Education', 'HOD', 'nkwineinnocent@gmail.com', '0785287421', 'inno', '123', 1),
-(2, 'Kulubasi', 'Laban', 'None', 'System admin', 'laban@gmail.com', '0785287422', 'lab', '123', 3),
-(3, '', '', 'agriculture', 'staff', 'nkwineinnocent@gmail.com', '0785287420', 'iga', '123', 2);
+INSERT INTO `users` (`id`, `fname`, `lname`, `department`, `position`, `email`, `contact`, `username`, `password`, `cod`, `org`) VALUES
+(1, 'Nkwine', 'Innocent', 'Education', 'HOD', 'nkwineinnocent@gmail.com', '0785287421', 'inno', '123', 1, 'muni'),
+(2, 'Kulubasi', 'Laban', 'None', 'System admin', 'laban@gmail.com', '0785287422', 'lab', '123', 3, 'muni'),
+(3, 'Iga', 'Isaac', 'agriculture', 'staff', 'nkwineinnocent@gmail.com', '0785287420', 'iga', '123', 2, 'muni');
 
 --
 -- Indexes for dumped tables
@@ -226,16 +211,16 @@ ALTER TABLE `applications`
   ADD PRIMARY KEY (`a_id`);
 
 --
--- Indexes for table `organizations`
---
-ALTER TABLE `organizations`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `publish`
 --
 ALTER TABLE `publish`
   ADD PRIMARY KEY (`a_id`);
+
+--
+-- Indexes for table `rating`
+--
+ALTER TABLE `rating`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `requests`
@@ -260,16 +245,16 @@ ALTER TABLE `applications`
   MODIFY `a_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `organizations`
---
-ALTER TABLE `organizations`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `publish`
 --
 ALTER TABLE `publish`
   MODIFY `a_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `rating`
+--
+ALTER TABLE `rating`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `requests`
