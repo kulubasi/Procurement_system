ef43fa3bd8cecd1689ca5f35285887baaf08a29c
IgaIsaac17 <igaisaaceb@gmail.com>
2022-11-11 02:52:42 +0300

    icon

--- pages/users.php
+++ pages/users.php
@@ -115,7 +115,7 @@ include("config.php");
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="../dist/img/FaceApp_164357366584_1.jpg" class="img-circle elevation-2 text-white" alt="User Image">
+          <img src="../dist/img/user_icon.png" class="img-circle elevation-2 text-white" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block"><?php $a =$_SESSION['$username']; echo $a; ?></a>
